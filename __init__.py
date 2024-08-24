26410cdf48ec47762f25faa18710f8415b689e65
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-08-24 21:15:53 +0900

    Add filepath node and update none values

--- __init__.py
+++ __init__.py
@@ -1,14 +1,21 @@
 from .nodes import *
 
+
 NODE_CLASS_MAPPINGS = {
     "LoadFasterWhisperModel": LoadFasterWhisperModel,
     "FasterWhisperTranscription": FasterWhisperTranscription,
     "FasterWhisperToSubtitle": FasterWhisperToSubtitle,
     "SaveSubtitle": SaveSubtitle,
+    "InputFilePath": InputFilePath,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
     "LoadFasterWhisperModel": "(Down)Load FasterWhisper Model",
     "FasterWhisperTranscription": "FasterWhisper Transcription",
     "FasterWhisperToSubtitle": "FasterWhisper To Subtitle",
-    "SaveSubtitle": "Save Subtitle",
+    "InputFilePath": "Input FilePath",
 }
+
+
+import os
+# Temporal fix of the bug : https://github.com/jhj0517/Whisper-WebUI/issues/144
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
--- nodes.py
+++ nodes.py
@@ -9,7 +9,11 @@ from comfy.utils import ProgressBar
 from .subtitle_utils import AVAILABLE_SUBTITLE_FORMAT, format_transcriptions_to_subtitle, get_incremented_filename
 
 faster_whisper_script_dir_path = os.path.dirname(os.path.abspath(__file__))
+faster_whisper_model_dir = os.path.join(folder_paths.models_dir, "faster-whisper")
 faster_whisper_output_dir_path = folder_paths.get_output_directory()
+
+FLOAT_NONE_VALUE = -999.0
+INT_NONE_VALUE = -999
 
 
 class LoadFasterWhisperModel:
@@ -46,13 +50,12 @@ class LoadFasterWhisperModel:
                    model: str,
                    device: str,
                    ) -> Tuple[faster_whisper.WhisperModel]:
-        model_dir = os.path.join(folder_paths.models_dir, "faster-whisper")
-        os.makedirs(model_dir, exist_ok=True)
+        os.makedirs(faster_whisper_model_dir, exist_ok=True)
 
         faster_whisper_model = faster_whisper.WhisperModel(
             device=device,
             model_size_or_path=model,
-            download_root=model_dir,
+            download_root=faster_whisper_model_dir,
         )
 
         return (faster_whisper_model, )
@@ -81,23 +84,23 @@ class FasterWhisperTranscription:
                 "no_repeat_ngram_size": ("INT", {"default": 0}),
                 "prefix": ("STRING", {"default": ""}),
                 "suppress_blank": ("BOOLEAN", {"default": True}),
-                "suppress_tokens": ("STRING", {"default": "-1"}),
+                "suppress_tokens": ("STRING", {"default": "[-1]"}),
                 "max_initial_timestamp": ("FLOAT", {"default": 1.0}),
                 "word_timestamps": ("BOOLEAN", {"default": False}),
                 "prepend_punctuations": ("STRING", {"default": "\"'“¿([{-"}),
                 "append_punctuations": ("STRING", {"default": "\"'.。,，!！?？:：”)]}、"}),
-                "max_new_tokens": ("INT", ),
-                "chunk_length": ("INT", ),
-                "hallucination_silence_threshold": ("FLOAT", ),
-                "hotwords": ("STRING", ),
-                "language_detection_threshold": ("FLOAT", ),
+                "max_new_tokens": ("INT", {"default": INT_NONE_VALUE}),
+                "chunk_length": ("INT", {"default": INT_NONE_VALUE}),
+                "hallucination_silence_threshold": ("FLOAT", {"default": FLOAT_NONE_VALUE}),
+                "hotwords": ("STRING", {"default": ""}),
+                "language_detection_threshold": ("FLOAT", {"default": FLOAT_NONE_VALUE}),
                 "language_detection_segments": ("INT", {"default": 1}),
                 "prompt_reset_on_temperature": ("FLOAT", {"default": 0.5}),
                 "condition_on_previous_text": ("BOOLEAN", {"default": True}),
-                "initial_prompt": ("STRING", ),
+                "initial_prompt": ("STRING", {"default": ""}),
                 "without_timestamps": ("BOOLEAN", {"default": False}),
                 "vad_filter": ("BOOLEAN", {"default": False}),
-                "vad_parameters": ("STRING", ),
+                "vad_parameters": ("STRING", {"default": ""}),
                 "clip_timestamps": ("STRING", {"default": "0"}),
             }
         }
@@ -108,12 +111,12 @@ class FasterWhisperTranscription:
     CATEGORY = "FASTERWHISPER"
 
     def transcribe(self,
-                   audio: Union[str, BinaryIO, np.ndarray],
+                   audio: Union[str, BinaryIO, np.ndarray, Dict],
                    model: faster_whisper.WhisperModel,
                    **params,
                    ) -> Tuple[List]:
-        if params["language"] == "auto":
-            params["language"] = None
+        audio = self.preprocess_audio(audio)
+        params = self.collect_params(params)
 
         segments, info = model.transcribe(
             audio=audio,
@@ -124,13 +127,45 @@ class FasterWhisperTranscription:
 
         transcriptions = []
         for segment in segments:
-            comfy_pbar.update_absolute(segment.start)
+            comfy_pbar.update_absolute(segment.end)
             transcriptions.append({
                 "start": segment.start,
                 "end": segment.end,
                 "text": segment.text
             })
         return (transcriptions, )
+
+    @staticmethod
+    def preprocess_audio(audio):
+        audio, sr = audio["waveform"], audio["sample_rate"]
+        audio = audio.detach().cpu().numpy()
+        return audio
+
+    @staticmethod
+    def collect_params(params):
+        # Set None values manually because this seems not allowed to use in ComfyUI
+        if "language" in params and params["language"] == "auto":
+            params["language"] = None
+        if "suppress_tokens" in params:
+            params["suppress_tokens"] = eval(params["suppress_tokens"])
+        if "prefix" in params and not params["prefix"]:
+            params["prefix"] = None
+        if "hotwords" in params and not params["hotwords"]:
+            params["hotwords"] = None
+        if "initial_prompt" in params and not params["initial_prompt"]:
+            params["initial_prompt"] = None
+        if "vad_parameters" in params and not params["vad_parameters"]:
+            params["vad_parameters"] = None
+        if "max_new_tokens" in params and params["max_new_tokens"] == INT_NONE_VALUE:
+            params["max_new_tokens"] = None
+        if "chunk_length" in params and params["chunk_length"] == INT_NONE_VALUE:
+            params["chunk_length"] = None
+        if "hallucination_silence_threshold" in params and params["hallucination_silence_threshold"] == FLOAT_NONE_VALUE:
+            params["hallucination_silence_threshold"] = None
+        if "language_detection_threshold" in params and params["language_detection_threshold"] == FLOAT_NONE_VALUE:
+            params["language_detection_threshold"] = None
+
+        return params
 
 
 class FasterWhisperToSubtitle:
@@ -144,7 +179,7 @@ class FasterWhisperToSubtitle:
         }
 
     RETURN_TYPES = ("SUBTITLE",)
-    RETURN_NAMES = ("subtitle",)
+    RETURN_NAMES = ("subtitle text",)
     FUNCTION = "format_to_subtitle"
     CATEGORY = "FASTERWHISPER"
 
@@ -188,3 +223,24 @@ class SaveSubtitle:
         with open(output_path, "w") as f:
             f.write(subtitle)
         return (output_path,)
+
+
+class InputFilePath:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "filepath": ("STRING", {"default": "", "multiline": False})
+            }
+        }
+
+    RETURN_TYPES = ("FILEPATH", )
+    RETURN_NAMES = ("filepath",)
+    FUNCTION = "process_filepath"
+    CATEGORY = "FASTERWHISPER"
+
+    def process_filepath(self, filepath):
+        if not os.path.exists(filepath):
+            raise ValueError(f"File not found: {filepath}")
+
+        return (filepath,)
