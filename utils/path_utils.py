6464e8390eefae4129fd73f590561ec1c5a6d130
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-11-02 03:38:48 +0900

    Create dir path

--- utils/path_utils.py
+++ utils/path_utils.py
@@ -3,7 +3,7 @@ import folder_paths
 import faster_whisper
 
 faster_whisper_model_dir = os.path.join(folder_paths.models_dir, "faster-whisper")
-
+os.makedirs(faster_whisper_model_dir, exist_ok=True)
 
 def collect_model_paths(
     model_dir: str = faster_whisper_model_dir,
